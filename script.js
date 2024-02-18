9d9b6f50506474b53ae84c0a8ffcf3dd214742d0
deXcoder9 <dhrubothejuniorprogrammer@gmail.com>
2024-02-18 17:36:24 +0600

    y

--- script.js
+++ script.js
@@ -13,47 +13,88 @@ let totalSeatDecrease = 40;
 let totalPrice = 0;
 let totalSeatSelected = 0;
 let grandTotal = 0;
+let totalbooked = 0;
 
 for (const seat of seats) {
-  seat.addEventListener("click", function (event) {
-    seat.classList.add("bg-[#1DD100]");
-    totalPrice += 550;
-    grandTotal += 550;
-    totalSeatSelected += 1;
-    console.log(totalPrice);
-    selectedSeat += 1;
-    // selected seat
-    const Sseat = document.getElementById("selected_seat_number");
-    Sseat.innerText = selectedSeat;
-    // seats leftover
-    const leftover = document.getElementById("totalSeatDecrease");
-    // let newSeatlleft =
-    totalSeatDecrease -= 1;
-    leftover.innerText = totalSeatDecrease;
-    // chosen seat display
-    const cosnSeat = document.getElementById("chosen_seats");
-    let newSeatContainer = document.createElement("div");
-    newSeatContainer.classList.add("flex");
-    newSeatContainer.classList.add("justify-between");
-    newSeatContainer.classList.add("pt-3");
+  seat.addEventListener("click", function selectSeat(event) {
+    seat.removeEventListener("click", selectSeat); // Remove the event listener
+    totalbooked += 1;
+    if (totalbooked <= 4) {
+      seat.classList.add("bg-[#1DD100]");
+      totalPrice += 550;
+      grandTotal += 550;
+      totalSeatSelected += 1;
+      selectedSeat += 1;
+      // selected seat
+      const Sseat = document.getElementById("selected_seat_number");
+      Sseat.innerText = selectedSeat;
+      // seats leftover
+      const leftover = document.getElementById("totalSeatDecrease");
+      // let newSeatlleft =
+      totalSeatDecrease -= 1;
+      leftover.innerText = totalSeatDecrease;
+      // chosen seat display
+      const cosnSeat = document.getElementById("chosen_seats");
+      let newSeatContainer = document.createElement("div");
+      newSeatContainer.classList.add("flex");
+      newSeatContainer.classList.add("justify-between");
+      newSeatContainer.classList.add("pt-3");
 
-    newSeatContainer.innerHTML = `
-    <h5 class="  text-sm "></h5>
-    <h5 class="  text-sm ">Economy</h5>
-    <h5 class="  text-sm ">550</h5>
-    `;
+      newSeatContainer.innerHTML = `
+      <h5 class="  text-sm "></h5>
+      <h5 class="  text-sm ">Economy</h5>
+      <h5 class="  text-sm ">550</h5>
+      `;
 
-    let SeatName = seat.innerText;
-    console.log(SeatName);
-    let i = newSeatContainer.querySelector("h5");
-    i.innerText = SeatName;
-    console.log(i);
-    // Grand total Price
-    const grand_total = document.getElementById("grand_total");
-    grand_total.innerText = grandTotal;
+      let SeatName = seat.innerText;
+      let i = newSeatContainer.querySelector("h5");
+      i.innerText = SeatName;
+      // Grand total Price
+      const grand_total = document.getElementById("grand_total");
+      grand_total.innerText = grandTotal;
 
-    cosnSeat.appendChild(newSeatContainer);
-    const total_price = document.getElementById("total_price");
-    total_price.innerText = totalPrice;
+      cosnSeat.appendChild(newSeatContainer);
+      const total_price = document.getElementById("total_price");
+      total_price.innerText = totalPrice;
+    } else {
+      alert("Sorry! You can't buy more than 4 tickets (its our policy)");
+    }
   });
+}
+
+function applyCoupon() {
+  const inputField = document.getElementById("input-field").value;
+  console.log(typeof inputField);
+  if (inputField === "NEW 15") {
+    document.getElementById("input-field").value = " ";
+
+    const DiscountPriceOfTotal = (totalPrice * 15) / 100;
+    totalPrice -= DiscountPriceOfTotal;
+    console.log(DiscountPriceOfTotal);
+    document
+      .getElementById("discountPriceContainer")
+      .classList.remove("hidden");
+    document.getElementById("discount_Price").innerText = DiscountPriceOfTotal;
+    document.getElementById("COupn_box").classList.add("hidden");
+    let leftMoney = grandTotal - DiscountPriceOfTotal;
+    document.getElementById("grand_total").innerText = leftMoney;
+  }
+  else if (inputField === "Couple 20") {
+    document.getElementById("input-field").value = " ";
+
+    const DiscountPriceOfTotal = (totalPrice * 20) / 100;
+    totalPrice -= DiscountPriceOfTotal;
+    console.log(DiscountPriceOfTotal);
+    document
+      .getElementById("discountPriceContainer")
+      .classList.remove("hidden");
+    document.getElementById("discount_Price").innerText = DiscountPriceOfTotal;
+    document.getElementById("COupn_box").classList.add("hidden");
+    let leftMoney = grandTotal - DiscountPriceOfTotal;
+    document.getElementById("grand_total").innerText = leftMoney;
+  }
+   else {
+    alert(" please put a valid coupon ");
+    document.getElementById("input-field").value = " ";
+  }
 }
